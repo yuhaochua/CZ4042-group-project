784c4cb72a7cda7e6bb147beabf90505d9154dcd
DESKTOP-RLI54N2\Yu_Hao <yuhaochua99@gmail.com>
2023-11-05 23:23:29 +0800

    add vgg and resnet

--- model.py
+++ model.py
@@ -1,5 +1,9 @@
 import torch.nn as nn
 import torch
+import torchvision.models as models
+from torchvision.ops import deform_conv2d
+from collections import OrderedDict
+import torch.optim as optim
 
 class CNN(nn.Module):
     def __init__(self):
@@ -23,3 +27,71 @@ class CNN(nn.Module):
         dropout = self.dropout(d1)
         out = torch.softmax(self.d2(dropout), dim=1)
         return conv1, p1, conv2, p2, out
+    
+def resnet(dropout=0.5):
+    model = models.resnet152(pretrained=True)
+    model_requires_grad_params = []
+
+    # Freeze parameters so we don't backprop through them
+    for param in model.parameters():
+        param.requires_grad = False
+
+    model.fc = nn.Sequential(OrderedDict([
+                          ('fc1', nn.Linear(2048, 102)),
+                          ('output', nn.LogSoftmax(dim=1))
+                          ]))
+    
+    # conv = deform_conv2d()
+
+    criterion = nn.NLLLoss()
+
+    optimizer = optim.Adam(
+    [
+        {'params': model.conv1.parameters(),  'lr': 0.000001},
+        {'params': model.layer1.parameters(), 'lr': 0.000001},
+        {'params': model.layer2.parameters(), 'lr': 0.00001},
+        {'params': model.layer3.parameters(), 'lr': 0.00001},
+        {'params': model.layer4.parameters(), 'lr': 0.0001},
+        {'params': model.fc.parameters(),     'lr': 0.001}
+    ], lr=0.0, weight_decay=0.001)
+
+    return model , optimizer ,criterion
+
+def vgg(dropout=0.5, lr = 0.001):
+    
+    model = models.vgg16(pretrained=True)
+        
+    for param in model.parameters():
+        param.requires_grad = False
+
+    classifier = nn.Sequential(OrderedDict([
+                        ('dropout1', nn.Dropout(dropout)),
+                        ('fc2', nn.Linear(25088, 102)),
+                        ('output', nn.LogSoftmax(dim=1))
+                        ]))
+    
+    model.classifier = classifier
+    criterion = nn.NLLLoss()
+    optimizer = optim.Adam(model.classifier.parameters(), lr)
+    
+    
+    return model , optimizer ,criterion
+    
+def mobilenet(dropout=0.5, lr = 0.001):
+    model = models.mobilenet_v2(pretrained=True)
+
+    for param in model.parameters():
+        param.requires_grad = False
+
+    classifier = nn.Sequential(OrderedDict([
+                        ('dropout1', nn.Dropout(dropout)),
+                        ('fc2', nn.Linear(1280, 102)),
+                        ('output', nn.LogSoftmax(dim=1))
+                        ]))
+    
+    model.classifier = classifier
+
+    criterion = nn.NLLLoss()
+    optimizer = optim.Adam(model.classifier.parameters(), lr)
+
+    return model , optimizer ,criterion
--- train.py
+++ train.py
@@ -4,9 +4,9 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import json
-import torchvision.models as models
 
-from model import CNN
+
+from model import CNN, vgg, resnet, mobilenet
 
 with open('./data/cat_to_name.json', 'r') as f:
     cat_to_name = json.load(f)
@@ -49,56 +49,42 @@ def eval(dataloader, model, device):
 
             correct += pred.eq(target.view_as(pred)).sum().item() # compare predicted label to actual label
     return correct / len(dataloader.dataset)
-
-def nn_setup(dropout=0.5, hidden_layer1 = 120,lr = 0.001):
     
-    model = models.vgg16(pretrained=True)  
-        
-    for param in model.parameters():
-        param.requires_grad = False
 
-        from collections import OrderedDict
-        classifier = nn.Sequential(OrderedDict([
-                          ('fc1', nn.Linear(25088, 500)),
-                          ('relu', nn.ReLU()),
-                          ('dropout1', nn.Dropout(dropout)),
-                          ('fc2', nn.Linear(500, 102)),
-                          ('output', nn.LogSoftmax(dim=1))
-                          ]))
-        
-        model.classifier = classifier
-        criterion = nn.NLLLoss()
-        optimizer = optim.Adam(model.classifier.parameters(), lr )
-        
-        
-        return model , optimizer ,criterion
 
 if __name__ == "__main__":
     loss_list = []
     accuracy_list = []
+    torch.manual_seed(0)
     train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
     val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False)
     test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
-    # model = CNN()
-    # criterion = nn.CrossEntropyLoss()
-    # optimizer = optim.AdamW(model.parameters(), lr=LR, weight_decay=1e-4)
-    model,optimizer,criterion = nn_setup()
+
+
+    # model,optimizer,criterion = vgg() # accuracy: 0.8002927....
+    # model,optimizer,criterion = resnet() # accuracy: 0.
+    model,optimizer,criterion = mobilenet()
     model.to(DEVICE)
-    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.5)
-    best_acc = 0
-    early_stop_count = 0
-    for epoch in range(1, NUM_EPOCH+1):
-        train_loss = train(train_loader, model, criterion, optimizer, DEVICE)
-        accuracy = eval(val_loader, model, DEVICE)
-        print(f'Epoch {epoch}, Train Loss: {train_loss}, Val Accuracy: {accuracy}')
-        if accuracy > best_acc:
-            best_acc = accuracy
-            early_stop_count = 0
-        else:
-            early_stop_count += 1
-        if early_stop_count >= EARLY_STOP_THRESHOLD:
-            print("Early Stopping...")        
-            break
-        scheduler.step()
-    test_accuracy = eval(test_loader, model, DEVICE)
-    print(f'Test Accuracy: {test_accuracy}')
+
+    for name, module in model.named_children():
+        print("NAME: ", name)
+        print("MODULE: ", module)
+
+    # scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.5)
+    # best_acc = 0
+    # early_stop_count = 0
+    # for epoch in range(1, NUM_EPOCH+1):
+    #     train_loss = train(train_loader, model, criterion, optimizer, DEVICE)
+    #     accuracy = eval(val_loader, model, DEVICE)
+    #     print(f'Epoch {epoch}, Train Loss: {train_loss}, Val Accuracy: {accuracy}')
+    #     if accuracy > best_acc:
+    #         best_acc = accuracy
+    #         early_stop_count = 0
+    #     else:
+    #         early_stop_count += 1
+    #     if early_stop_count >= EARLY_STOP_THRESHOLD:
+    #         print("Early Stopping...")        
+    #         break
+    #     scheduler.step()
+    # test_accuracy = eval(test_loader, model, DEVICE)
+    # print(f'Test Accuracy: {test_accuracy}')
